3e944d78e88fba26cf8eda71b63fbacda1a9c76e
QuantumSheep <quantumsheep@outlook.fr>
2018-07-31 14:24:56 +0200

    Add more lennys and commands ( ͡◉ ͜ʖ ͡◉)

--- app.js
+++ app.js
@@ -3,24 +3,66 @@ const config = require("./config.json");
 
 const client = new Discord.Client();
 
+/**
+ * Try to delete a message
+ * 
+ * @param {Discord.Message} msg 
+ */
+const tryDelete = msg => {
+    if (msg.deletable) {
+        msg.delete();
+    }
+}
+
+const lennys = {
+    'default': '( ͡° ͜ʖ ͡°)',
+    'tripping': '( ͡◉ ͜ʖ ͡◉)',
+    'sensual': '(͠≖ ͜ʖ͠≖)',
+    'communist': '(☭ ͜ʖ ☭)',
+    'nothappy': '( ͡° ʖ̯ ͡°)'
+}
+
 client.on('ready', () => {
     console.log('I am ready!');
-});
 
-client.on('roleUpdate', (oldRole, newRole) => {
-    console.log(oldRole, newRole);
-})
+    const helpEmbed = new Discord.RichEmbed({
+        color: '000000',
+        title: "Lenny's help",
+        description: `Type \`/lenny\` or mention me (<@${client.user.id}>) to access all lenny's commands.`,
+        fields: [
+            {
+                name: 'help',
+                value: `Open the help information panel (the one you're looking at right now)`
+            },
+            {
+                name: 'Types of lenny',
+                value: Object.keys(lennys).join(', ')
+            }
+        ]
+    });
 
-client.on('message', msg => {
-    if (msg.author.bot) return;
-
-    if (msg.content.indexOf(config.prefix) === 0 || msg.isMentioned(client.user)) {
-        if (msg.deletable) {
-            msg.delete();
+    client.on('message', msg => {
+        if (msg.author.bot) return;
+    
+        if (msg.content.indexOf(config.prefix) === 0 || msg.content.indexOf(`<@${client.user.id}>`) === 0) {
+            const args = msg.content.trim().split(/ +/g);
+            const command = args.shift().toLowerCase();
+    
+            if (args[0]) {
+                if (args[0] === 'help') {
+                    msg.channel.send(helpEmbed);
+                } else if (args[0] in lennys) {
+                    msg.channel.send(lennys[args[0]]);
+                } else {
+                    msg.channel.send('This lenny is unknown ( ͡° ʖ̯ ͡°)')
+                }
+            } else {
+                msg.channel.send('( ͡° ͜ʖ ͡°)');
+            }
+    
+            tryDelete(msg);
         }
-
-        msg.channel.send('( ͡° ͜ʖ ͡°)');
-    }
+    });
 });
 
 client.on('error', err => {
