d5c491447d7d50502242ab1b3fd731ba4ee25e93
quantumsheep <nathanael.dmc@outlook.fr>
2020-12-16 21:10:40 +0100

    Update invitation link with slash commands scope

--- app.js
+++ app.js
@@ -79,7 +79,7 @@ client.on('ready', () => {
       },
       {
         name: 'Invitation link',
-        value: `<https://bit.ly/2x47KIB>`
+        value: `<https://bit.ly/2KxnOwh>`
       },
       {
         name: 'Source code',
